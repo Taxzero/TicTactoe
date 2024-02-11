3b87d2fa2d047d78d7fcffebac4dee6bea51800d
Moon <syoung90@naver.com>
2024-02-11 17:55:04 +0900

    'update-css'

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
     'Go to move #' + move : 'Go to game start';
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button className="move-button" onClick={() => jumpTo(move)}>{desc}</button>
       </li>
     )
   })
@@ -75,7 +75,7 @@ function App() {
       </div>
       <div className='game-info'>
         <div className='status'>{status}</div>
-        <ol>{moves}</ol>
+        <ol style={{listStyle: 'none'}}>{moves}</ol>
       </div>
     </div>
   );
